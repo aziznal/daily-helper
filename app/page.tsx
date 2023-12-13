6c19a4ec06eb1d9baaa967201489ae2c89333da4
Aziz <aziznal.dev@gmail.com>
2023-12-13 22:22:51 +0300

    setup supabase and basic page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
+"use client";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
 export default function Home() {
+  const supabase = createClientComponentClient();
+
   return (
     <div className="flex justify-center items-center h-full">
       <h1 className="text-5xl">Hello World!</h1>
