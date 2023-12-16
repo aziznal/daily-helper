48835020f79e5d5986074c9e764cb655a8b9ad46
aziznal <aziznal.dev@gmail.com>
2023-12-16 15:16:21 +0300

    add missing return statement in password check

--- app/enter-password/page.tsx
+++ app/enter-password/page.tsx
@@ -81,6 +81,10 @@ export default function EnterPasswordPage() {
         description: "Please try again.",
         variant: "destructive",
       });
+
+      setIsSubmitting(false);
+
+      return;
     }
 
     toast({
